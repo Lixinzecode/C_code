69c703f1dfbfc8ebb0be44b66edfcae7ff9cb529
linxinzecode <2394372860@qq.com>
2025-06-26 21:57:17 +0800

    mem

--- String_And_Memory/String_And_Memory/sterror.c
+++ String_And_Memory/String_And_Memory/sterror.c
@@ -1,9 +1,9 @@
-#define _CRT_SECURE_NO_WARNINGS 1
-#include "test.h"
-
-int main()
-{
-	errno;
-
-	return 0;
-}
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include "test.h"
+//
+//int main()
+//{
+//	errno;
+//
+//	return 0;
+//}
